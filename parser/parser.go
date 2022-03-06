a8b2babd71ebbd81da13715e34e02de377d68162
coffeecup <IgYmB0218LP@gmail.com>
2022-03-06 16:25:41 +0900

    done 2.5

--- parser/parser.go
+++ parser/parser.go
@@ -57,6 +57,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -74,6 +76,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// Todo 
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// Todo
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
