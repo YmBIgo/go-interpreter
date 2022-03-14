f6c8247b0fb5182e57aa2d79f9edb0597f051056
coffeecup <IgYmB0218LP@gmail.com>
2022-03-14 14:17:39 +0900

    done 2.8.6

--- parser/parser.go
+++ parser/parser.go
@@ -149,10 +149,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// Todo 
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
+
+	// Todo 
+	// for !p.curTokenIs(token.SEMICOLON) {
+	// 	p.nextToken()
+	// }
 
 	return stmt
 }
@@ -162,8 +170,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// Todo
-	for !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,39 +8,72 @@ import (
 )
 
 func TestLetStatements(t *testing.T) {
-	input := `
-let x = 5;
-let y = 10;
-let foobar = 838383;
-	`
-	l := lexer.New(input)
-	p := New(l)
+// 	input := `
+// let x = 5;
+// let y = 10;
+// let foobar = 838383;
+// 	`
+// 	l := lexer.New(input)
+// 	p := New(l)
 
-	program := p.ParseProgram()
-	checkParserErrors(t, p)
+// 	program := p.ParseProgram()
+// 	checkParserErrors(t, p)
 
-	if program == nil {
-		t.Fatalf("ParseProgram() return nil")
-	}
-	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
-			len(program.Statements))
-	}
+// 	if program == nil {
+// 		t.Fatalf("ParseProgram() return nil")
+// 	}
+// 	if len(program.Statements) != 3 {
+// 		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+// 			len(program.Statements))
+// 	}
+
+// 	tests := []struct {
+// 		expectedIdentifier string
+// 	}{
+// 		{"x"},
+// 		{"y"},
+// 		{"foobar"},
+// 	}
+
+// 	for i, tt := range tests {
+// 		stmt := program.Statements[i]
+// 		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+// 			return
+// 		}
+// 	}
 
 	tests := []struct {
-		expectedIdentifier string
+		input 				string
+		expectedIdentifier	string
+		expectedValue 		interface{}
 	}{
-		{"x"},
-		{"y"},
-		{"foobar"},
+		{"let x = 5;", "x", 5},
+		{"let y = true;", "y", true},
+		{"let foobar = y", "foobar", "y"},
 	}
 
-	for i, tt := range tests {
-		stmt := program.Statements[i]
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements does not contain %d statements. got=%d",
+				1, len(program.Statements))
+		}
+
+		stmt := program.Statements[0]
 		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
 			return
 		}
+
+		val := stmt.(*ast.LetStatement).Value
+		if !testLiteralExpression(t, val, tt.expectedValue) {
+			return
+		}
 	}
+
 }
 
 func checkParserErrors(t *testing.T, p *Parser) {
@@ -644,7 +677,6 @@ func TestCallExpressionParsing(t *testing.T) {
 	testInfixExpression(t, exp.Arguments[1], 2, "*", 3)
 	testInfixExpression(t, exp.Arguments[2], 4, "+", 5)
 }
-
 
 
 
